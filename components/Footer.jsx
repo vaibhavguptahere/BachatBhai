a6fec44a16711afad04ee462c5c7fe6a4f65b8b7
Vaibhav Gupta <guptavaibhavg2005@gmail.com>
2025-07-03 21:35:54 +0530

    Day-2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='bg-blue-50 py-6'>
+        <div className='bg-blue-50 py-6 border-t'>
             <div className="container mx-auto px-4 text-center text-grey-600">
-                Made with ❤️ by <span className='font-bold'><a href="https://www.linkedin.com/in/vaibhavguptahere-/"></a>Vaibhav Gupta</span>
+                Made with ❤️ by <span className='font-bold'><a href="https://www.linkedin.com/in/vaibhavguptahere-/" target='blank'>VAIBHAV GUPTA</a></span>
             </div>
         </div>
     )
