008635d8f1daeeff1527266f1e2a2e514bc276a5
Vaibhav Gupta <guptavaibhavg2005@gmail.com>
2025-07-24 08:01:37 +0530

    Back to Track Again

--- app/(main)/account/[id]/page.js
+++ app/(main)/account/[id]/page.js
@@ -1,47 +1,55 @@
-import React, { Suspense } from 'react'
-import { getAccountWithTransactions } from '@/actions/account'
-import { notFound } from 'next/navigation';
-import TransactionTable from '../_components/transaction-table';
-import { BarLoader } from 'react-spinners';
-const AccountsPage = async ({ params }) => {
+import { Suspense } from "react";
+import { getAccountWithTransactions } from "@/actions/account";
+import { BarLoader } from "react-spinners";
+import TransactionTable  from "../_components/transaction-table";
+import { notFound } from "next/navigation";
+// import { AccountChart } from "../_components/account-chart";
+
+export default async function AccountPage({ params }) {
   const accountData = await getAccountWithTransactions(params.id);
 
   if (!accountData) {
     notFound();
   }
+
   const { transactions, ...account } = accountData;
 
   return (
-    <div className='pt-28 space-y-8 px-5 '>
-
-      {/* Main Account Show Section */}
-      <div className="gap-4 items-end justify-between flex">
-        <div className="">
-          <h1 className='text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize'>{account.name}</h1>
-          <p className='text-muted-foreground'>
-            {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+    <div className="space-y-8 px-5 mt-22">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
           </p>
-        </div >
+        </div>
 
         <div className="text-right pb-2">
           <div className="text-xl sm:text-2xl font-bold">
-            Rs {parseFloat(account.balance).toFixed(2)}
+            Rs. {parseFloat(account.balance).toFixed(2)}
           </div>
-          <p className='text-sm text-muted-foreground'>
+          <p className="text-sm text-muted-foreground">
             {account._count.transactions} Transactions
           </p>
         </div>
       </div>
 
       {/* Chart Section */}
+      {/* <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions} />
+      </Suspense> */}
 
-      {/* Transaction Table */}
-      <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='#9333ea' />}>
-        <TransactionTable transactions={accountData.transactions}/>
+      {/* Transactions Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <TransactionTable transactions={transactions} />
       </Suspense>
-
     </div>
-  )
+  );
 }
-
-export default AccountsPage
