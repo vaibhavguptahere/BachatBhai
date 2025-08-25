c705db9ca1f53a692bd1be5b2c8ac559448c73b6
Vaibhav Gupta <guptavaibhavg2005@gmail.com>
2025-08-25 14:47:52 +0530

    trigger recc trans. added

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import { checkBudgetAlert, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        checkBudgetAlert
+        checkBudgetAlert, triggerRecurringTransactions, processRecurringTransaction
     ],
 });
