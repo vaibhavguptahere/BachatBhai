c7478dfcd6b7d099e173fa08050a41c4b33daffc
Vaibhav Gupta <guptavaibhavg2005@gmail.com>
2025-08-27 16:16:04 +0530

    Pie chart ui fix

--- app/(main)/dashboard/_components/dashboard-overview.jsx
+++ app/(main)/dashboard/_components/dashboard-overview.jsx
@@ -149,15 +149,15 @@ const DashboardOverview = ({ accounts, transactions }) => {
                             No expenses this month
                         </p>
                     ) : (
-                        <div className="h-64"> {/* fixed height for chart */}
+                        <div className="h-58"> {/* fixed height for chart */}
                             <ResponsiveContainer width="100%" height="100%">
                                 <PieChart>
                                     <Pie
                                         data={pieChartData}
                                         cx="50%"
                                         cy="50%"
-                                        innerRadius={80}
-                                        outerRadius={100}
+                                        innerRadius={70}
+                                        outerRadius={90}
                                         label={({ name, value }) => `${name}: Rs. ${value.toFixed(2)}`}
                                         fill="#8884d8"
                                         dataKey="value"
